92d23d2ecf8f71e1fefc114c50bd060b9a5d7768
cozzyy2002 <cozzyy2002@gmail.com>
2016-10-10 18:21:57 +0900

    Add DMO enumeration.

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -6,20 +6,34 @@
 #define INITGUID
 #include <guiddef.h>
 #include <mfapi.h>
+#include <mftransform.h>
+#include <dmoreg.h>
+
+#include <log4cplus/configurator.h>
 
 static HRESULT enumMFTransforms();
+static HRESULT enumDMOs();
 
 int main()
 {
+	log4cplus::BasicConfigurator::doConfigure();
+
+	CoInitializeEx(NULL, COINIT_MULTITHREADED);
+	MFStartup(MF_VERSION);
+
 	enumMFTransforms();
+	enumDMOs();
+
+	MFShutdown();
+	CoUninitialize();
 
     return 0;
 }
 
-#define GUID_ENTRY(x) { x, #x }
+#define GUID_ENTRY(x) { x, OLESTR(#x) }
 struct GuidEntry {
 	REFGUID guid;
-	LPTSTR name;
+	LPCWSTR name;
 };
 
 static const GuidEntry g_mftCategories[] = {
@@ -35,25 +49,70 @@ static const GuidEntry g_mftCategories[] = {
 	GUID_ENTRY(MFT_CATEGORY_VIDEO_PROCESSOR),
 };
 
+static OLECHAR strGuid[40];
+
 HRESULT enumMFTransforms()
 {
 	for (int i = 0; i < ARRAYSIZE(g_mftCategories); i++) {
 		const GuidEntry& entry = g_mftCategories[i];
+		StringFromGUID2(entry.guid, strGuid, ARRAYSIZE(strGuid));
 		CLSID* pClsId;
 		UINT32 count;
 		HR_ASSERT_OK(MFTEnum(entry.guid, 0, NULL, NULL, NULL, &pClsId, &count));
 
-		std::cout << entry.name << ": Count=" << count << std::endl;
+		std::wcout << L"\n" << strGuid << L":" << entry.name << L": Count=" << count << std::endl;
 
 		for (UINT32 n = 0; n < count; n++) {
 			LPWSTR name;
 			if (SUCCEEDED(MFTGetInfo(pClsId[n], &name, NULL, NULL, NULL, NULL, NULL))) {
-				std::wcout << L"  " << n << L": " << name << std::endl;
+				StringFromGUID2(pClsId[n], strGuid, ARRAYSIZE(strGuid));
+				std::wcout << L"  " << n << L": " << strGuid << L":" << name << std::endl;
 				CoTaskMemFree(name);
 			}
 		}
 
 		CoTaskMemFree(pClsId);
+	}
+
+	return S_OK;
+}
+
+static const GuidEntry g_dmoCategories[] = {
+	GUID_ENTRY(DMOCATEGORY_AUDIO_DECODER),			// Audio decoder
+	GUID_ENTRY(DMOCATEGORY_AUDIO_EFFECT),			// Audio effect
+	GUID_ENTRY(DMOCATEGORY_AUDIO_ENCODER),			// Audio encoder
+	GUID_ENTRY(DMOCATEGORY_VIDEO_DECODER),			// Video decoder
+	GUID_ENTRY(DMOCATEGORY_VIDEO_EFFECT),			// Video effect
+	GUID_ENTRY(DMOCATEGORY_VIDEO_ENCODER),			// Video encoder
+	GUID_ENTRY(DMOCATEGORY_AUDIO_CAPTURE_EFFECT),	// Audio capture effect
+};
+
+HRESULT enumDMOs()
+{
+	for (int i = 0; i < ARRAYSIZE(g_dmoCategories); i++) {
+		const GuidEntry& entry = g_dmoCategories[i];
+
+		StringFromGUID2(entry.guid, strGuid, ARRAYSIZE(strGuid));
+		std::wcout << L"\n" << strGuid << L":" << entry.name << std::endl;
+
+		CComPtr<IEnumDMO> enumDmo;
+		HR_ASSERT_OK(DMOEnum(entry.guid, DMO_ENUMF_INCLUDE_KEYED,
+						0, NULL,
+						0, NULL,
+						&enumDmo));
+		while (true) {
+			GUID guid;
+			WCHAR* pStr;
+			HRESULT hr = HR_EXPECT_OK(enumDmo->Next(1, &guid, &pStr, NULL));
+			if (hr != S_OK) break;
+			CComPtr<IMediaObject> dmo;
+			HR_ASSERT_OK(dmo.CoCreateInstance(guid));
+			CComPtr<IMFTransform> mft;
+			hr = dmo.QueryInterface(&mft);
+			StringFromGUID2(guid, strGuid, ARRAYSIZE(strGuid));
+			std::wcout << L"  " << strGuid << L":" << (SUCCEEDED(hr) ? L"MFT " : L"    ") << pStr << std::endl;
+			CoTaskMemFree(pStr);
+		}
 	}
 
 	return S_OK;
