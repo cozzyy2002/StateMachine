0b520895076dced0ae88bf6296451722fd0b5810
cozzyy2002 <cozzyy2002@gmail.com>
2017-08-15 20:01:02 +0900

    Add StateMachineApp MFC project.  * $(SolutionDir)PropertySheet has been added.

---
+++ StateMachineApp/StateMachineApp.h
@@ -0,0 +1,45 @@
+
+// StateMachineApp.h : main header file for the StateMachineApp application
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"       // main symbols
+
+
+// CStateMachineAppApp:
+// See StateMachineApp.cpp for the implementation of this class
+//
+
+class CStateMachineAppApp : public CWinAppEx
+{
+public:
+	CStateMachineAppApp();
+
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// Implementation
+protected:
+	HMENU  m_hMDIMenu;
+	HACCEL m_hMDIAccel;
+
+public:
+	BOOL  m_bHiColorIcons;
+
+	virtual void PreLoadState();
+	virtual void LoadCustomState();
+	virtual void SaveCustomState();
+
+	afx_msg void OnAppAbout();
+	afx_msg void OnFileNew();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CStateMachineAppApp theApp;
